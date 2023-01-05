b83ee7d1853ddbf5d566dc61220e7f8f133aad39
Mohammad Hassieb <mohammadhassieb@gmail.com>
2023-01-05 19:50:14 +0200

    .

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,3 @@
-/*!
-* Start Bootstrap - Resume v7.0.5 (https://startbootstrap.com/theme/resume)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
 
 window.addEventListener('DOMContentLoaded', event => {
 
